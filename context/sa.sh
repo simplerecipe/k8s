c076d99b7c650c7290f2fc0b29448a6c68a4892f
Alexey Moskalenko <48552864+vnesebya@users.noreply.github.com>
2021-11-22 16:41:52 +0300

    Create sa.sh

---
+++ context/sa.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$1" == "" ];then
+    echo "Usage: $0 secretname"
+    exit 1
+fi
+name=$1
+
+echo $0 $name
+
+ca=$(kubectl get secret/$name -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl get secret/$name -o jsonpath='{.data.token}' | base64 --decode)
+namespace=$(kubectl get secret/$name -o jsonpath='{.data.namespace}' | base64 --decode)
+
+echo "
+apiVersion: v1
+kind: Config
+clusters:
+- name: default-cluster
+  cluster:
+    certificate-authority-data: ${ca}
+    server: ${server}
+contexts:
+- name: default-context
+  context:
+    cluster: default-cluster
+    namespace: default
+    user: default-user
+current-context: default-context
+users:
+- name: default-user
+  user:
+    token: ${token}
+" > $name.kubeconfig
